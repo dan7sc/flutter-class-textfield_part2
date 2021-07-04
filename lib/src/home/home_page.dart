a87c663c02b724f1e2a913c05d87dc27d3666350
dan7sc <su7cheng@gmail.com>
2021-07-04 11:27:52 -0300

    Wrap Column with all CustomTextField in a Form

--- lib/src/home/home_page.dart
+++ lib/src/home/home_page.dart
@@ -54,19 +54,21 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              CustomTextField(
-                textInputAction: TextInputAction.next,
-                labelText: "Nome",
-                controller: _nameController,
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              CustomTextField(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CustomTextField(
+                  textInputAction: TextInputAction.next,
+                  labelText: "Nome",
+                  controller: _nameController,
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                CustomTextField(
                   textInputAction: TextInputAction.next,
                   labelText: "Telefone",
                   controller: _phoneController,
@@ -76,86 +78,89 @@ class _MyHomePageState extends State<MyHomePage> {
                   },
                   inputFormatters: [
                     LengthLimitingTextInputFormatter(14),
-                  ]),
-              SizedBox(
-                height: 8.0,
-              ),
-              CustomTextField(
-                textInputAction: TextInputAction.next,
-                labelText: "Data de Nascimento",
-                controller: _bornDateController,
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              CustomTextField(
-                textInputAction: TextInputAction.next,
-                labelText: "E-mail",
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              CustomTextField(
-                textInputAction: TextInputAction.next,
-                labelText: "CPF",
-                controller: _cpfController,
-                keyboardType: TextInputType.number,
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              CustomTextField(
-                textInputAction: TextInputAction.next,
-                labelText: "Senha",
-                controller: _passwordController,
-                focusNode: _passwordFocusNode,
-                obscureText: passwordVisible,
-                suffixIcon: VisibleWidget(
-                  visible: passwordVisible,
-                  onPressed: () {
-                    setState(() {
-                      passwordVisible = !passwordVisible;
-                    });
-                  },
+                  ],
                 ),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              CustomTextField(
-                textInputAction: TextInputAction.next,
-                labelText: "Confirmar Senha",
-                controller: _confirmPasswordController,
-                obscureText: confirmPasswordVisible,
-                suffixIcon: VisibleWidget(
-                  visible: confirmPasswordVisible,
-                  onPressed: () {
-                    setState(() {
-                      confirmPasswordVisible = !confirmPasswordVisible;
-                    });
-                  },
+                SizedBox(
+                  height: 8.0,
                 ),
-              ),
-              SizedBox(
-                height: 80.0,
-              ),
-              Flex(
-                direction: Axis.horizontal,
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        FocusScope.of(context).unfocus();
-                      },
-                      child: Text("Criar conta"),
-                    ),
+                CustomTextField(
+                  textInputAction: TextInputAction.next,
+                  labelText: "Data de Nascimento",
+                  controller: _bornDateController,
+                  keyboardType: TextInputType.number,
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                CustomTextField(
+                  textInputAction: TextInputAction.next,
+                  labelText: "E-mail",
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                CustomTextField(
+                  textInputAction: TextInputAction.next,
+                  labelText: "CPF",
+                  controller: _cpfController,
+                  keyboardType: TextInputType.number,
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                CustomTextField(
+                  textInputAction: TextInputAction.next,
+                  labelText: "Senha",
+                  controller: _passwordController,
+                  focusNode: _passwordFocusNode,
+                  obscureText: passwordVisible,
+                  suffixIcon: VisibleWidget(
+                    visible: passwordVisible,
+                    onPressed: () {
+                      setState(() {
+                        passwordVisible = !passwordVisible;
+                      });
+                    },
                   ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                CustomTextField(
+                  textInputAction: TextInputAction.next,
+                  labelText: "Confirmar Senha",
+                  controller: _confirmPasswordController,
+                  obscureText: confirmPasswordVisible,
+                  suffixIcon: VisibleWidget(
+                    visible: confirmPasswordVisible,
+                    onPressed: () {
+                      setState(() {
+                        confirmPasswordVisible = !confirmPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 80.0,
+                ),
+                Flex(
+                  direction: Axis.horizontal,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _formKey.currentState?.validate();
+                          FocusScope.of(context).unfocus();
+                        },
+                        child: Text("Criar conta"),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
